728236471153985c9f305448e42edd171b836848
Bravo <gmb2388@yahoo.com>
2021-01-24 14:12:21 -0800

    moved weather cards side by side

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,9 +1,8 @@
 var apiKey = "28545911e32e1e789ebcf427787efdec";
-var url = "api.openweathermap.org/data/2.5/";
+var baseUrl = "https://api.openweathermap.org/data/2.5/";
 var searchBtn = document.getElementById("button-addon2");
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-// api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid={API key}
 
 $(searchBtn).on("click", function () {
     $("#currentInfo").empty();
@@ -12,7 +11,7 @@ $(searchBtn).on("click", function () {
     var userInput = $("#user-input").val();
     $("#user-input").empty();
 
-    apiCall = url + "forecast?q=" + userInput + "&appid=" + apiKey;
+    apiCall = baseUrl + "find?q=" + userInput + "&appid=" + apiKey;
     console.log(apiCall);
     var history = [];
     history.push(userInput);
@@ -27,7 +26,7 @@ $(searchBtn).on("click", function () {
             $("#currentInfo").empty();
             $("#fiveDayWth").empty();
             var repeatCity = $(this).attr("data-city");
-            var repeatUrl = url + "forecast?q=" + repeatCity + "&appid=" + apiKey;
+            var repeatUrl = baseUrl + "forecast?q=" + repeatCity + "&appid=" + apiKey;
             displayWeather(repeatUrl);
         });
         displayWeather(apiCall);
@@ -45,12 +44,13 @@ function displayWeather(apiCall) {
         var longitude = response.list[0].coord.lon;
 
         var formattedDate = convertUnixTimestamp(response.list[0].dt);
-        locationApiCall = Url + "onecall?lat=" + latitide + "&lon=" + longitude + "&exclude=minutely,hourly&appid=" + apiKey + '&units=imperial';
+        locationApiCall = baseUrl + "onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly&appid=" + apiKey + '&units=imperial';
         var initialResponse = response;
         $.ajax({
             url: locationApiCall,
             method: "GET"
         }).then(function (response) {
+            console.log(response);
 
             var city = document.createElement("h1");
             city.innerText = initialResponse.list[0].name;
@@ -61,7 +61,7 @@ function displayWeather(apiCall) {
             $("#currentInfo").append(temperature);
 
             var hum = document.createElement("p");
-            humidity.innerText = "Humidity: " + response.current.humidity;
+            hum.innerText = "Humidity: " + response.current.humidity;
             $("#currentInfo").append(hum);
 
             var windSpeed = document.createElement("p");
@@ -72,7 +72,7 @@ function displayWeather(apiCall) {
             uvIndex.innerText = "UV Index: " + response.current.uvi;
             var uviDisplay = response.current.uvi;
             $(uvIndex).attr("id", "uvi");
-            $("#current info").append(uvIndex);
+            $("#currentInfo").append(uvIndex);
 
             if (uviDisplay <= 2) {
                 document.getElementById("uvi").style.color = "green";
@@ -88,7 +88,7 @@ function displayWeather(apiCall) {
 
             };
             var fiveDay = [0, 1, 2, 3, 4]
-            for (var i = 0; i < array.length; i++) {
+            for (var i = 0; i < fiveDay.length; i++) {
                 var elementDate = convertUnixTimestamp(response.daily[i].dt);
                 var weatherIcon = response.daily[i].weather[0].icon;
                 var elementTemp = response.daily[i].temp.day;
@@ -96,8 +96,8 @@ function displayWeather(apiCall) {
                 var elementDay = response.daily[i];
 
                 var fiveDayDiv = document.createElement("div");
-                $(fiveDayDiv).addClass("card text-white bg-primary mb-3 col-sm-2");
-                $("#fiveDayWth")
+                $(fiveDayDiv).addClass("card text-white bg-primary mb-3 col-sm-12 col-lg-2 col-md-4");
+                $("#fiveDayWth").append(fiveDayDiv);
 
                 var headerDiv = document.createElement("div");
                 $(headerDiv).addClass("card-header");
@@ -113,11 +113,13 @@ function displayWeather(apiCall) {
                 bodyDiv.append(iconImg);
 
                 var tempParagraph = document.createElement("p");
-                tempParagraph.innerText = "temperature: " + elementTemp;
+                tempParagraph.innerText = "Temp: " + elementTemp;
+                tempParagraph.style.fontSize = '14px';
                 bodyDiv.append(tempParagraph);
 
                 var humidityParagraph = document.createElement("p");
-                humidityParagraph.innerText = "humidity: " + elementHumidity;
+                humidityParagraph.innerText = "Humidity: " + elementHumidity;
+                humidityParagraph.style.fontSize = '14px';
                 bodyDiv.append(humidityParagraph);
             }
 
